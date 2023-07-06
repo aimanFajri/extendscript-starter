533b9f53130b49667307ebaa32f854bb3ecc9a3c
aimanFajri <emanmcr16@gmail.com>
2023-07-06 11:32:34 +0800

    Update extendscript-config.js

--- extendscript-config.js
+++ extendscript-config.js
@@ -1,10 +1,10 @@
-module.exports = {
-  tsEntry: __dirname + '/src/AfterEffects/tsconfig.json',
-  jsEntry: __dirname + '/build/aeft/index.js',
-  dest: '/Applications/Adobe After Effects CC 2018/Scripts/ScriptUI Panels/ExtendScript Starter.jsx',
-  logServerPort: 8081,
-  devConnectPort: 8080
-}
+//module.exports = {
+//  tsEntry: __dirname + '/src/AfterEffects/tsconfig.json',
+//  jsEntry: __dirname + '/build/aeft/index.js',
+ // dest: '/Applications/Adobe After Effects CC 2018/Scripts/ScriptUI Panels/ExtendScript Starter.jsx',
+ // logServerPort: 8081,
+ // devConnectPort: 8080
+//}
 
 // module.exports = {
 //   tsEntry: __dirname + '/src/Audition/tsconfig.json',
@@ -14,13 +14,13 @@ module.exports = {
 //   devConnectPort: 8080
 // }
 
-// module.exports = {
-//   tsEntry: __dirname + '/src/Illustrator/tsconfig.json',
-//   jsEntry: __dirname + '/build/ilst/index.js',
-//   dest: '/Applications/Adobe Illustrator CC 2018/Presets.localized/en_GB/Scripts/ExtendScript Starter.jsx',
-//   logServerPort: 8081,
-//   devConnectPort: 8080
-// }
+ module.exports = {
+  tsEntry: __dirname + '/src/Illustrator/tsconfig.json',
+  jsEntry: __dirname + '/build/ilst/index.js',
+  dest: '/Applications/Adobe Illustrator CC 2020/Presets.localized/en_GB/Scripts/ExtendScript Starter.jsx',
+   logServerPort: 8081,
+  devConnectPort: 8080
+ }
 
 // module.exports = {
 //   tsEntry: __dirname + '/src/InDesign/tsconfig.json',
